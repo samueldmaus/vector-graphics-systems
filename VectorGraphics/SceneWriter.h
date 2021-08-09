362c2cca760e48d64590b02ae1d7296398cbf476
Sam <samueldmaus@gmail.com>
2021-08-09 01:49:31 -0500

    scene writer almost finished

---
+++ VectorGraphics/SceneWriter.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "Element.h"
+#include "Scene.h"
+
+namespace Framework
+{
+	class SceneWriter
+	{
+	public:
+		SceneWriter() = default;
+		
+		SceneWriter(const SceneWriter& other) = default;
+		SceneWriter(SceneWriter&& other) noexcept = default;
+		~SceneWriter() = default;
+
+		SceneWriter& operator=(const SceneWriter&) = default;
+		SceneWriter& operator=(SceneWriter&&) = default;
+
+		static Xml::HElement writeScene(Scene& scene);
+	};
+}
