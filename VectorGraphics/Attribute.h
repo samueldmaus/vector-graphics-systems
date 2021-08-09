af0289224174758ac75329d9ec449ee410cec876
Sam <samueldmaus@gmail.com>
2021-08-08 23:19:15 -0500

    add attribute tests

--- VectorGraphics/Attribute.h
+++ VectorGraphics/Attribute.h
@@ -9,6 +9,13 @@ namespace Xml
 	public:
 		Attribute(std::string n, std::string v);
 
+		Attribute(const Attribute& other) = default;
+		Attribute(Attribute&& other) noexcept = default;
+		~Attribute() = default;
+
+		Attribute& operator=(const Attribute&) = default;
+		Attribute& operator=(Attribute&&) = default;
+
 		[[nodiscard]] const std::string& getName() const;
 		[[nodiscard]] const std::string& getValue() const;
 
