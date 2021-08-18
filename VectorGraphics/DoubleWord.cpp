9932fff7c3060b6d723c3fe66b6d9635da7ecaa8
Sam <samueldmaus@gmail.com>
2021-08-18 00:53:05 -0500

    double word added

---
+++ VectorGraphics/DoubleWord.cpp
@@ -0,0 +1,37 @@
+﻿#include "DoubleWord.h"
+#include "Word.h"
+
+namespace Binary
+{
+	DoubleWord::DoubleWord(uint32_t initialValue) : value(initialValue)
+	{
+	}
+	
+	DoubleWord::operator uint32_t() const
+	{
+		return value;
+	}
+	
+	DoubleWord& DoubleWord::operator=(uint32_t newValue)
+	{
+		this->value = newValue;
+		return *this;
+	}
+
+	DoubleWord DoubleWord::readLittleEndian(std::istream& sourceStream)
+	{
+		const uint16_t tmp1 = Word::readLittleEndian(sourceStream);
+		const uint16_t tmp2 = Word::readLittleEndian(sourceStream);
+		const uint32_t start = (static_cast<uint32_t>(tmp2) << 16) | tmp1;
+		return DoubleWord(start);
+	}
+
+	DoubleWord DoubleWord::readBigEndian(std::istream& sourceStream)
+	{
+		const uint16_t tmp1 = Word::readBigEndian(sourceStream);
+		const uint16_t tmp2 = Word::readBigEndian(sourceStream);
+		const uint32_t start = (static_cast<uint32_t>(tmp1) << 16) | tmp2;
+		return DoubleWord(start);
+	}
+	
+}
---
+++ VectorGraphics/DoubleWord.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <cstdint>
+#include <istream>
+
+namespace Binary
+{
+	class DoubleWord
+	{
+	public:
+		explicit DoubleWord(uint32_t initialValue = 0);
+
+		DoubleWord(const DoubleWord&) = default;
+		DoubleWord(DoubleWord&&) = default;
+		~DoubleWord() = default;
+
+		DoubleWord& operator=(const DoubleWord&) = default;
+		DoubleWord& operator=(DoubleWord&&) noexcept = default;
+
+		operator uint32_t () const;
+		DoubleWord& operator=(uint32_t newValue);
+
+		static DoubleWord readLittleEndian(std::istream& sourceStream);
+		static DoubleWord readBigEndian (std::istream& sourceStream);
+	
+	private:
+		uint32_t value;
+	};
+}
