9a0caf4413d790fa6141922da643dd1dc94931a2
Sam <samueldmaus@gmail.com>
2021-08-19 00:06:56 -0500

    starting on windosbitmapheader

---
+++ VectorGraphics/WindowsBitmapHeader.cpp
@@ -0,0 +1,23 @@
+﻿#include "WindowsBitmapHeader.h"
+
+namespace BitmapGraphics
+{
+	void WindowsBitmapHeader::readFileHeader(std::istream& sourceStream)
+	{
+		//verifyEquality(firstIdentifier, Binary::Byte::read(sourceStream), "firstIdentifier");
+		//verifyEquality(secondIdentifier, Binary::Byte::read(sourceStream), "secondIdentifier");
+		//this->fileSize = Binary::DoubleWord::readLittleEndian(sourceStream);
+		//verifyEquality(reserved, Binary::DoubleWord::readLittleEndian(sourceStream), "reserved");
+		//verifyEquality(rawImageByteOffset, Binary::DoubleWord::readLittleEndian(sourceStream), "rawImageByteOffset");
+	}
+
+	void WindowsBitmapHeader::writeFileHeader(std::ostream& destinationStream) const
+	{
+		firstIdentifier.write(destinationStream);
+		secondIdentifier.write(destinationStream);
+		fileSize.writeLittleEndian(destinationStream);
+		reserved.writeLittleEndian(destinationStream);
+		rawImageByteOffset.writeLittleEndian(destinationStream);
+	}
+
+}
---
+++ VectorGraphics/WindowsBitmapHeader.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "Byte.h"
+#include "DoubleWord.h"
+#include "Word.h"
+
+namespace BitmapGraphics
+{
+	class WindowsBitmapHeader
+	{
+	public:
+		// appropriate accessors, constructors, etc. . .
+		// . . .
+
+		void readFileHeader(std::istream& sourceStream);
+		void writeFileHeader(std::ostream& destinationStream) const;
+
+	private:
+		// file header
+		const static Binary::Byte firstIdentifier;
+		const static Binary::Byte secondIdentifier;
+		Binary::DoubleWord fileSize;
+		const static Binary::DoubleWord reserved;
+		const static Binary::DoubleWord rawImageByteOffset;
+
+		// info header
+		const static Binary::DoubleWord infoHeaderBytes;
+		Binary::DoubleWord bitmapWidth;
+		Binary::DoubleWord bitmapHeight;
+		const static Binary::Word numberOfPlanes;
+		const static Binary::Word bitsPerPixel;
+		const static Binary::DoubleWord compressionType;
+		const static Binary::DoubleWord compressedImageSize;
+		const static Binary::DoubleWord horizontalPixelsPerMeter;
+		const static Binary::DoubleWord verticalPixelsPerMeter;
+		const static Binary::DoubleWord numberOfColors;
+		const static Binary::DoubleWord numberOfImportantColors;
+	};
+}
